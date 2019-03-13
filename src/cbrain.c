6cf0476dc6c3a843efbb635f26d21dfbeda90679
Auxilus <raghavsphadke@gmail.com>
2019-03-13 18:09:40 +0000

    get number of neurons for threads from argv instead of hardcoding them

--- src/cbrain.c
+++ src/cbrain.c
@@ -17,7 +17,7 @@ int main(int argc, char* argv[])
 
 	struct brain* b = brain_init((uint)neurons_no);
 	neuron_link_random(b);
-	struct nthread* nt1 = thread_struct_new(0, 999);
+	struct nthread* nt1 = thread_struct_new(0, neurons_no-1);
 	struct nthread* at  = thread_struct_new(0, 0);
 	thread_create(nt1, b, 0, sleep_t);
 	thread_create(at, b, 1, sleep_t);
